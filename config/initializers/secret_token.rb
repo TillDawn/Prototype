81f75866ac0dd7ea8c401efff5289321752db896
Theodore Wilson <tedawilson@gmail.com>
2012-04-22 23:33:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TillDawn::Application.config.secret_token = 'b674388f2a127c67e69ddbbed0f77a494c5ba82b788e437034f2ef97d612b5e51532040c26d3d21bee06157d68e3add98b22f3bd61cc90d5b8aaeb9c3874c5eb'
